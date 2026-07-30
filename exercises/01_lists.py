7354fb9f6507567901e04fb1a979d26812e2b5f7
Alistair Kung <alistaircckung@gmail.com>
2026-07-30 14:54:10 +0800

    Set up Python training project

---
+++ exercises/01_lists.py
@@ -0,0 +1,9 @@
+numbers = [10, 20, 30]
+
+print(numbers)
+print(numbers[0])
+print(numbers[-1])
+
+numbers.append(40)
+
+print(numbers)
---
+++ tests/test_setup.py
@@ -0,0 +1,2 @@
+def test_python_is_working():
+    assert 1 + 1 == 2
